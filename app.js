14f51a4d0188e1cf9110ae0fa0ceb1e8240042e4
ychatter <borocky46@gmail.com>
2023-09-09 00:33:43 +0400

    changes

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const messageInput = document.getElementById("message-input");
     const chatMessages = document.getElementById("chat-messages");
     const sendButton = document.getElementById("send-button");
-    const logoutButton = document.getElementById("logout-button");
 
     let username = ""; // Store the username
     let avatarURL = ""; // Store the avatar image URL
@@ -23,8 +22,6 @@ document.addEventListener("DOMContentLoaded", function () {
             messageInput.disabled = false;
             sendButton.disabled = false;
             attachImageButton.disabled = false;
-            logoutButton.disabled = false; // Enable the logout button
-            logIPAddress(); // Log the user's IP address when they set a username
         }
     }
 
@@ -36,51 +33,84 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // ... (other code) ...
-
-    // Function to log the user's IP address
-    function logIPAddress() {
-        fetch("log.php", {
-            method: "POST", // You can use GET or POST depending on your server configuration
-        })
-        .then(response => {
-            if (response.ok) {
-                console.log("IP address logged successfully.");
-            } else {
-                console.error("Failed to log IP address.");
-            }
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
+    // Function to open the file input dialog for uploading avatars
+    function openAvatarInput() {
+        avatarInput.click();
     }
 
-    // Event listener for the logout button
-    logoutButton.addEventListener("click", () => {
-        logout();
-    });
+    // Event listener for opening the avatar input dialog
+    uploadAvatarButton.addEventListener("click", openAvatarInput);
 
-    // Function to handle the logout action
-    function logout() {
-        if (username !== "") {
-            // Display a "has left the chat" message and clear the user's data
-            const leaveMessage = `<div class="message info">${username} has left the chat</div>`;
-            chatMessages.innerHTML += leaveMessage;
-
-            // Clear the username and avatar data
-            username = "";
-            avatarURL = "";
-
-            // Disable input and buttons
-            usernameInput.disabled = false;
-            setUsernameButton.disabled = false;
-            messageInput.disabled = true;
-            sendButton.disabled = true;
-            attachImageButton.disabled = true;
-            logoutButton.disabled = true; // Disable the logout button
-
-            // Clear the message input
-            messageInput.value = "";
+    // Function to handle avatar image selection
+    function handleAvatarSelection(event) {
+        const file = event.target.files[0];
+        if (file) {
+            avatarURL = URL.createObjectURL(file);
+            // You can display the selected avatar image if needed
         }
     }
+
+    // Event listener for handling avatar image selection
+    avatarInput.addEventListener("change", handleAvatarSelection);
+
+    // Function to open the file input dialog for attaching images to messages
+    function openFileInput() {
+        fileInput.click();
+    }
+
+    // Event listener for opening the file input dialog
+    attachImageButton.addEventListener("click", openFileInput);
+
+    // Function to handle image selection and send messages
+    function handleImageSelection(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const imageURL = URL.createObjectURL(file);
+            sendMessage(`<img src="${imageURL}" alt="Image" />`);
+            fileInput.value = "";
+        }
+    }
+
+    // Event listener for handling image selection and sending messages
+    fileInput.addEventListener("change", handleImageSelection);
+
+    // Function to send a message
+    function sendMessage(messageText) {
+        if (messageText) {
+            chatMessages.innerHTML += `<div class="message sent"><img src="${avatarURL}" alt="Avatar" class="user-avatar">${username}: ${messageText}</div>`;
+            messageInput.value = "";
+            chatMessages.scrollTop = chatMessages.scrollHeight;
+        }
+    }
+
+    // Event listener for sending messages
+    sendButton.addEventListener("click", () => {
+        const message = messageInput.value.trim();
+        sendMessage(message);
+    });
+
+    // Event listener for pressing Enter key in the message input
+    messageInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            const message = messageInput.value.trim();
+            sendMessage(message);
+        }
+    });
+
+    // Function to request camera permission
+    function requestCameraPermission() {
+        navigator.mediaDevices
+            .getUserMedia({ video: true })
+            .then((stream) => {
+                console.log("Camera access granted");
+                stream.getTracks().forEach((track) => track.stop());
+            })
+            .catch((error) => {
+                console.error("Camera access denied or error:", error);
+            });
+    }
+
+    // Event listener for requesting camera permission
+    attachImageButton.addEventListener("click", requestCameraPermission);
 });
+
